fed545fe04a20ab5fec264d2e29e841e4d9cfad9
surepos <125876053+surepos@users.noreply.github.com>
2025-06-18 09:43:25 +0900

    Update Dashboard.tsx

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -20,7 +20,7 @@ export function SidebarDemo({ children }: { children: React.ReactNode }) {
       label: "Dashboard",
       href: "/",
       icon: (
-        <IconBrandTabler className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+        <IconBrandTabler className="h-5 w-5 shrink-0 text-neutral-200" />
       ),
     },
     {
